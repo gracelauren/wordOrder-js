3c4a2fffec4aed998d6d596db0995d8f42553976
gracelauren <gracelauren909@gmail.com>
2015-02-12 22:18:49 -0800

    test shorten

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,12 @@ var wordOrder = function(userString) {
   newArray.forEach(function(userWord) {
     if (/[,.!?"":;]/.test(userWord)) {
       userWord = userWord.replace(/[,.!?"":;]/g,"");
-        if (counter[userWord] === undefined) {
-          counter[userWord] = 1;
-        } else {
-          counter[userWord] += 1;
-        }
+    } 
+    if (counter[userWord] === undefined) {
+      counter[userWord] = 1;
     } else {
-      if (counter[userWord] === undefined) {
-        counter[userWord] = 1;
-      } else {
-        counter[userWord] += 1;
-      }
+      counter[userWord] += 1;
+    }
     };
 
 
